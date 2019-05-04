05936d43a2a84917fcd00cb63e6129133a650a87
boosmoke <lybeck.john@gmail.com>
2019-05-04 09:14:08 -0700

    file

---
+++ custom.js
@@ -0,0 +1,31 @@
+function addScrollto(element){
+  let clone = document.querySelector('.Index-page-scroll-indicator').cloneNode( true );
+  //clone.setAttribute( 'class', 'Index-page-scroll-indicator custom-scroll' );
+  document.querySelector(element).appendChild( clone );
+
+  function isScrolledIntoView(el) {
+    var rect = el.getBoundingClientRect();
+    var elemTop = rect.top;
+    var elemBottom = rect.bottom;
+
+    // Only completely visible elements return true:
+    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    // Partially visible elements return true:
+    //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+    //if in view hide
+    if(isVisible && !el.classList.contains('prevent')){
+      el.classList.add('prevent')
+      setTimeout(function(){ el.classList.add('hidden'); }, 500);
+      console.log('hhh')
+    //if scrolled above div only show element
+    }else if((elemTop > window.innerHeight)){
+      el.classList.remove('prevent')
+      el.classList.remove('hidden');
+    }
+    return isVisible;
+  }
+
+  document.addEventListener('scroll', function(){
+    isScrolledIntoView(clone)
+  })
+}
