d14d5ca3f4949717a6ff6b9e1feb0fbf3375e3e0
boosmoke <lybeck.john@gmail.com>
2019-05-04 09:17:32 -0700

    small update

--- custom.js
+++ custom.js
@@ -1,31 +1,39 @@
-function addScrollto(element){
-  let clone = document.querySelector('.Index-page-scroll-indicator').cloneNode( true );
-  //clone.setAttribute( 'class', 'Index-page-scroll-indicator custom-scroll' );
-  document.querySelector(element).appendChild( clone );
+document.addEventListener('DOMContentLoaded', function(){ 
+    // your code goes here
+    //example add to mens section. Use the id of the banner
+    // addScrollTo('#mens-de')
 
-  function isScrolledIntoView(el) {
-    var rect = el.getBoundingClientRect();
-    var elemTop = rect.top;
-    var elemBottom = rect.bottom;
 
-    // Only completely visible elements return true:
-    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
-    // Partially visible elements return true:
-    //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
-    //if in view hide
-    if(isVisible && !el.classList.contains('prevent')){
-      el.classList.add('prevent')
-      setTimeout(function(){ el.classList.add('hidden'); }, 500);
-      console.log('hhh')
-    //if scrolled above div only show element
-    }else if((elemTop > window.innerHeight)){
-      el.classList.remove('prevent')
-      el.classList.remove('hidden');
+    function addScrollto(element){
+    let clone = document.querySelector('.Index-page-scroll-indicator').cloneNode( true );
+    //clone.setAttribute( 'class', 'Index-page-scroll-indicator custom-scroll' );
+    document.querySelector(element).appendChild( clone );
+
+    function isScrolledIntoView(el) {
+        var rect = el.getBoundingClientRect();
+        var elemTop = rect.top;
+        var elemBottom = rect.bottom;
+
+        // Only completely visible elements return true:
+        var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+        // Partially visible elements return true:
+        //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+        //if in view hide
+        if(isVisible && !el.classList.contains('prevent')){
+        el.classList.add('prevent')
+        setTimeout(function(){ el.classList.add('hidden'); }, 500);
+        console.log('hhh')
+        //if scrolled above div only show element
+        }else if((elemTop > window.innerHeight)){
+        el.classList.remove('prevent')
+        el.classList.remove('hidden');
+        }
+        return isVisible;
     }
-    return isVisible;
-  }
 
-  document.addEventListener('scroll', function(){
-    isScrolledIntoView(clone)
-  })
-}
+    document.addEventListener('scroll', function(){
+        isScrolledIntoView(clone)
+    })
+    }
+
+}, false);
